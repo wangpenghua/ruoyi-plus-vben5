dfbab164ecd427401785f731d771fcd5f1ebd69f
dap <15891557205@163.com>
2026-01-28 13:56:33 +0800

    debug: 调试使用 sourcemap

--- internal/vite-config/src/config/common.ts
+++ internal/vite-config/src/config/common.ts
@@ -5,7 +5,7 @@ async function getCommonConfig(): Promise<UserConfig> {
     build: {
       chunkSizeWarningLimit: 2000,
       reportCompressedSize: false,
-      sourcemap: false,
+      sourcemap: true,
     },
   };
 }
